ddf62fbe33b7762a05c8063acc9b7c10244c14d1
dennis <dennis.mwangi@student.moringaschool.com>
2024-02-14 06:06:59 +0300

    Completed lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,29 @@
 def badge_maker(name):
-    return None
+    badge_name = f"Hello, my name is {name}."
+    return badge_name
+
 
 def batch_badge_creator(names):
-    return None
+    altered_names = list()
+    for n in names:
+        altered_names.append(f"Hello, my name is {n}.")
+    return altered_names
+    
 
 def assign_rooms(names):
-    return None
+    room_assigned = list()
+    index = 1
+    for n in names:
+        room_assigned.append(f"Hello, {n}! You'll be assigned to room {index}!")
+        index += 1
+    return room_assigned
 
 def printer(names):
-    return None
+    badge_list = batch_badge_creator(names)
+    assign_rooms_list = assign_rooms(names)
+    for n in badge_list:
+        print(n)
+    for i in assign_rooms_list:
+        print(i)
+    return badge_list, assign_rooms_list
+print(printer(["Dennis","Gituhu"]))
